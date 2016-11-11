3c09832660379a138c0adbf15ae5af7981d9ee91
Sani Yusuf <saniyusuf@gmail.com>
2016-11-11 01:50:41 +0000

    added RTL

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController, Platform } from 'ionic-angular';
+import { Platform } from 'ionic-angular';
 
 @Component({
   selector: 'page-contact',
